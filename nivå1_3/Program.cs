9cbd74dd0e61aedf2e2fba7b413b67da772c543e
Hugo Ljungqvist <hulj07001@utb.vaxjo.se>
2024-12-11 13:33:29 +0100

    Add project files.

---
+++ nivå1_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace nivå1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int summa = 0;
+
+            for (int i = 1; i <=5; i++)
+            {
+                Console.WriteLine("Skriv in heltal nummer "+i+":");
+                int tal = int.Parse(Console.ReadLine());
+                summa += tal;
+            }
+            Console.WriteLine("Summar av de talen blir: " + summa + ".");
+        }
+    }
+}
